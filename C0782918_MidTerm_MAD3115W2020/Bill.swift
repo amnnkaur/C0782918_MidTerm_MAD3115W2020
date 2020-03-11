852648000c8eff396c9b2c1ab6bb7bb5ecb49e7f
Aman Kaur <amankaur2628@gmail.com>
2020-03-11 18:09:19 -0400

    [Aman] Class Bill added (variables and init)

---
+++ C0782918_MidTerm_MAD3115W2020/Bill.swift
@@ -0,0 +1,30 @@
+//
+//  Bill.swift
+//  C0782918_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-11.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Bill {
+    
+    var billId : Int
+    var billDate : String
+    //var amount : Float
+    enum bTypes: String {
+        case Internet
+        case Mobile
+        case Hydro
+    }
+    var billType : bTypes
+    
+    init(billId : Int , billDate : String, billType : bTypes) {
+        self.billId = billId
+        self.billDate = billDate
+        self.billType = billType
+    }
+    
+    
+}
