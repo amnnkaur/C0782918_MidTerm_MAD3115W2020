12e07c0894cd75dbaa99abd63faa73378b009db2
Aman Kaur <amankaur2628@gmail.com>
2020-03-11 18:10:38 -0400

    [Aman] Protocol IDisplay added

---
+++ C0782918_MidTerm_MAD3115W2020/IDisplay.swift
@@ -0,0 +1,14 @@
+//
+//  IDisplay.swift
+//  C0782918_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-11.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplay {
+    
+    func display()
+}
